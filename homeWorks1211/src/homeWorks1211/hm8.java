3d4fe7af518c299b579234d668686d7efbb84fe3
MRong138 <m13897998967@163.com>
2016-12-15 19:57:17 +0800

    Signed-off-by: MRong138<m13897998967@163.com> <MRong138<m13897998967@163.com>>

---
+++ homeWorks1211/src/homeWorks1211/hm8.java
@@ -0,0 +1,36 @@
+package homeWorks1211;
+import java.io.IOException;
+public class hm8 {
+
+	public static void main(String[] args) {
+		int i=0,n=0;
+		boolean flag=true;
+		char ch,a[]=new char[20];
+		System.err.println("请输入字符串或数字（最多20个）：");
+		try {
+			ch=(char)System.in.read();
+		
+		while(ch!='\n'){
+			a[n]=ch;
+			n++;
+			ch=(char)System.in.read();
+		}
+		}catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+
+		for (int j = 0; j <n/2; j++) {
+			if(a[j]!=a[n-2-j])
+				flag=false;
+			break;
+		}
+		
+		if(flag)
+			System.out.println("这是回文");
+		else
+			System.out.println("不是回文");
+	}
+
+}
+
