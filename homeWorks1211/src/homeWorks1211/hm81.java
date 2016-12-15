b02f2ae04ed20fa3f3c68f2b7dc41dd12330999d
MRong138 <m13897998967@163.com>
2016-12-15 20:25:20 +0800

    Signed-off-by: MRong138<m13897998967@163.com> <MRong138<m13897998967@163.com>>

---
+++ homeWorks1211/src/homeWorks1211/hm81.java
@@ -0,0 +1,24 @@
+package homeWorks1211;
+
+import java.util.Scanner;
+public class hm81 {
+
+	public static void main(String[] args) {
+		boolean flag=true;
+		System.out.println("请输入：");
+	    Scanner sc=new Scanner(System.in);
+	    String s=sc.next();
+	    char[] h=s.toCharArray();
+		for (int j = 0; j <h.length/2; j++) {
+			if(h[j]!=h[h.length-1-j])
+				flag=false;
+			break;
+		}
+		
+		if(flag)
+			System.out.println("这是回文");
+		else
+			System.out.println("不是回文");
+	}
+}
+
