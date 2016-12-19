d5b5ea8303a541724b23f23ef7d6cef92f4f0b60
MRong138 <m13897998967@163.com>
2016-12-19 23:51:14 +0800

    Signed-off-by: MRong138<m13897998967@163.com> <MRong138<m13897998967@163.com>>

--- homework1219/src/MianXiangDuiXiang/players.java
+++ homework1219/src/MianXiangDuiXiang/players.java
@@ -29,7 +29,7 @@ public class players {
 		System.out.println("请输入你的身高：");
 		Scanner sc=new Scanner(System.in);
 		int height=sc.nextInt();
-		Play p=null;    //老师为什么这儿得是null?
+		Play p=null;    //老师，为什么这儿得是null?
 		if(height>200)
 			p=new Play1();
 		else if(height<200&&height>150)
